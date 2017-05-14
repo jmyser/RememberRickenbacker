9faca955130ee47a2a72d032fa23a533d19604d0
jmyser <jmyser@gmail.com>
2017-05-14 01:09:33 -0400

    Got device accelerometer controls working!

--- RememberRickenbacker/GameScene.swift
+++ RememberRickenbacker/GameScene.swift
@@ -8,10 +8,14 @@
 
 import SpriteKit
 import GameplayKit
+import CoreMotion
 
 class GameScene: SKScene {
 
     let player = SKSpriteNode(imageNamed: "goodGuy005")
+    var motionManager = CMMotionManager()
+    var destX:CGFloat  = 0.0
+    var destY:CGFloat  = 0.0
     
     override func didMove(to view: SKView) {
         
@@ -21,11 +25,24 @@ class GameScene: SKScene {
         background.zPosition = 0
         self.addChild(background)
         
-        
         player.setScale(0.75)
-        player.position = CGPoint(x: self.size.width/2, y: self.size.height * 0.2)
+        player.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
         player.zPosition = 2
         self.addChild(player)
+        
+        if motionManager.isAccelerometerAvailable == true {
+            // 2
+            motionManager.startAccelerometerUpdates(to: OperationQueue.current!, withHandler:{
+                data, error in
+                
+                let currentX = self.player.position.x
+                let currentY = self.player.position.y
+                self.destX = currentX + CGFloat((data?.acceleration.x)! * 750)
+                self.destY = currentY + CGFloat((data?.acceleration.y)! * 750)
+                
+            })
+            
+        }
     }
     
     func fireBlaster() {
@@ -46,4 +63,13 @@ class GameScene: SKScene {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         fireBlaster()
     }
+    
+    override func update(_ currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+        var action = SKAction.moveTo(x: destX, duration: 0.5)
+        self.player.run(action)
+        action = SKAction.moveTo(y: destY, duration: 0.5)
+        self.player.run(action)
+    }
+    
 }
