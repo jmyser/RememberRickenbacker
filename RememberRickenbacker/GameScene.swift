934429fdd5b1b0268ca65ce38f4d7809cb3bdb84
jmyser <jmyser@gmail.com>
2017-05-15 00:38:02 -0400

    Trying to keep the ship on the screen, not working yet

--- RememberRickenbacker/GameScene.swift
+++ RememberRickenbacker/GameScene.swift
@@ -17,6 +17,22 @@ class GameScene: SKScene {
     var destX:CGFloat  = 0.0
     var destY:CGFloat  = 0.0
     
+    var gameArea: CGRect
+    
+    override init(size: CGSize) {
+        
+        let maxAspectRatio: CGFloat = 16.0/9.0
+        let playableWidth = size.height / maxAspectRatio
+        let margin = (size.width - playableWidth) / 2
+        gameArea = CGRect(x: margin, y: 0, width: playableWidth, height: size.height)
+        
+        super.init(size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func didMove(to view: SKView) {
         
         let background = SKSpriteNode (imageNamed: "backgroundIMG")
@@ -25,23 +41,21 @@ class GameScene: SKScene {
         background.zPosition = 0
         self.addChild(background)
         
-        player.setScale(0.75)
+        player.setScale(0.6)
         player.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
         player.zPosition = 2
         self.addChild(player)
         
         if motionManager.isAccelerometerAvailable == true {
-            // 2
             motionManager.startAccelerometerUpdates(to: OperationQueue.current!, withHandler:{
                 data, error in
                 
                 let currentX = self.player.position.x
                 let currentY = self.player.position.y
-                self.destX = currentX + CGFloat((data?.acceleration.x)! * 750)
-                self.destY = currentY + CGFloat((data?.acceleration.y)! * 750)
+                self.destX = currentX + CGFloat((data?.acceleration.x)! * 1500)
+                self.destY = currentY + CGFloat((data?.acceleration.y)! * 1500)
                 
             })
-            
         }
     }
     
@@ -70,6 +84,19 @@ class GameScene: SKScene {
         self.player.run(action)
         action = SKAction.moveTo(y: destY, duration: 0.5)
         self.player.run(action)
+        
+        if player.position.x > gameArea.maxX {
+            player.position.x = gameArea.maxX
+        }
+        if player.position.x < gameArea.minX {
+            player.position.x = gameArea.minX
+        }
+        if player.position.y > gameArea.maxY {
+            player.position.y = gameArea.maxY
+        }
+        if player.position.y < gameArea.minY {
+            player.position.y = gameArea.minY
+        }
     }
     
 }
